0849f0f9fecf1563a8bf62424c7cdaa98c026000
brandon3055 <brandon4261@gmail.com>
2025-04-30 18:10:07 +1000

    Simplified IInteractTile

--- src/main/java/com/brandon3055/brandonscore/lib/IInteractTile.java
+++ src/main/java/com/brandon3055/brandonscore/lib/IInteractTile.java
@@ -9,6 +9,8 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 
+import javax.annotation.Nullable;
+
 /**
  * Implement this on a tile that uses {@link BlockBCore} and you will be able to receive block use and block attack calls.
  * Created by brandon3055 on 21/10/2016.
@@ -16,24 +18,19 @@ import net.minecraft.world.phys.BlockHitResult;
 public interface IInteractTile {
 
     /**
-     * Generic use method that gets called when block is right-clicked with or without an item.
+     * Passed through from Block#useItemOn, Despite what the name may suggest, this method gets called regardless of weather the player is holding an item or not.
+     * The stack will simply be empty if the player is not holding an item.
      */
-    default InteractionResult useGeneric(BlockState state, Player player, BlockHitResult hit) {
-        return InteractionResult.FAIL;
-    }
-
     default ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Player player, InteractionHand hand, BlockHitResult hit) {
-        return switch (useGeneric(state, player, hit)) {
-            case SUCCESS, SUCCESS_NO_ITEM_USED -> ItemInteractionResult.SUCCESS;
-            case CONSUME -> ItemInteractionResult.CONSUME;
-            case CONSUME_PARTIAL -> ItemInteractionResult.CONSUME_PARTIAL;
-            case PASS -> ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
-            case FAIL -> ItemInteractionResult.FAIL;
-        };
+        return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
     }
 
+    /**
+     * Passed through from Block#useWithoutItem, also, just like useItemOn, this gets called regardless of weather the player is holding an item.
+     * But this does only get called if the interaction was not consumed by useItemOn, which gets called first.
+     */
     default InteractionResult useWithoutItem(BlockState state, Player player, BlockHitResult hit) {
-        return useGeneric(state, player, hit);
+        return InteractionResult.PASS;
     }
 
     /**
